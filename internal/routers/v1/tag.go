8984234e2efe5b252f012e58ee4ae36a911fb9e0
terry1207 <a4w94@yahoo.com.tw>
2023-04-05 17:26:54 +0800

    finish new server

---
+++ internal/routers/v1/tag.go
@@ -0,0 +1,28 @@
+package v1
+
+import "github.com/gin-gonic/gin"
+
+type Tag struct {
+}
+
+func NewTag() Tag {
+	return Tag{}
+}
+
+func (r Tag) Get(c *gin.Context) {
+
+}
+
+func (r Tag) List(c *gin.Context) {
+
+}
+
+func (r Tag) Create(c *gin.Context) {
+
+}
+func (r Tag) Update(c *gin.Context) {
+
+}
+func (r Tag) Delete(c *gin.Context) {
+
+}
